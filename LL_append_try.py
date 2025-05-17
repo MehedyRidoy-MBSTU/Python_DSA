6a3939bca74a07966011254997eb5ef2acdcd4ce
Ridoy <ridoy16024@gmail.com>
2025-05-17 22:22:49 +0600

    appending to a LL

--- LL_append_try.py
+++ LL_append_try.py
@@ -1,8 +1,7 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
-        
+        self.next = None  
 
 class LinkedList:
     def __init__(self, value):
@@ -11,18 +10,15 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    # Print method to display the linked list
     def print_list(self):
         temp = self.head
         while temp is not None:
-            print(temp.value, end="->")
+            print(temp.value, end=" -> ")
             temp = temp.next
         print("None")
-            
-    def make_empty(self):
-        self.head = None
-        self.tail = None
-        self.length = 0
-        
+
+    # Append method to add a new node at the end of the linked list
     def append(self, value):
         new_node = Node(value)
         if self.length == 0:
@@ -33,33 +29,10 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
-
-
-
+###########################################
 
 my_linked_list = LinkedList(1)
-my_linked_list.make_empty()
-
-my_linked_list.append(1)
 my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.print_list()  # Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> None
 
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length, '\n')
-
-print('Linked List:')
-my_linked_list.print_list()
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Head: 1
-    Tail: 2
-    Length: 2 
-
-    Linked List:
-    1
-    2
-    
-"""
